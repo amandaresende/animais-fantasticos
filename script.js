2790775eb1e1ed774eb8baa54ba9ce33486a1f23
Amanda Resende <amandacamposresende@gmail.com>
2022-12-13 09:41:34 -0300

    feat: Implementação animação ao scroll

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ initTabNav();
 
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo'
+  const activeClass = 'ativo';
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
@@ -37,3 +37,44 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible)
+          section.classList.add('ativo');
+
+      })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
